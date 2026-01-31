a6a69dd12fc0c14fabc8156dde577f7102fcf7d0
Jeff Li <jeff.li@ca.ibm.com>
2026-01-31 17:44:13 -0500

    Fix diff-scope.sh to use @{upstream} for branch-agnostic diff

--- ci/diff-scope.sh
+++ ci/diff-scope.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Simple diff to get changed directories
-SCOPE=$(git diff --name-only origin/main...HEAD | cut -d/ -f1 | sort -u)
+SCOPE=$(git diff --name-only @{upstream}...HEAD | cut -d/ -f1 | sort -u)
 export SCOPE
 echo "Changed scope: $SCOPE"
 export SCOPE_HASH=$(echo $SCOPE | md5sum | cut -d' ' -f1)
